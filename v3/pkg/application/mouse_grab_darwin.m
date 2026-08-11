6a55ecc65002c2f066a9a22d4f769e329f7e0330
Tim Linde <tim@linde.io>
2026-08-11 23:04:14 +0200

    feat(application): expose native relative mouse grab

---
+++ v3/pkg/application/mouse_grab_darwin.m
@@ -0,0 +1,41 @@
+#import <Cocoa/Cocoa.h>
+#import <ApplicationServices/ApplicationServices.h>
+
+extern void wailsMouseGrabDelta(double dx, double dy);
+
+static id wailsMouseGrabMonitor = nil;
+static NSWindow *wailsMouseGrabWindow = nil;
+static BOOL wailsMouseGrabbed = NO;
+
+void wails_mouse_grab_start(void *windowPtr) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (wailsMouseGrabbed) return;
+        wailsMouseGrabbed = YES;
+        wailsMouseGrabWindow = (__bridge NSWindow *)windowPtr;
+        CGAssociateMouseAndMouseCursorPosition(false);
+        CGDisplayHideCursor(kCGDirectMainDisplay);
+        NSEventMask mask = NSEventMaskMouseMoved | NSEventMaskLeftMouseDragged |
+            NSEventMaskRightMouseDragged | NSEventMaskOtherMouseDragged;
+        wailsMouseGrabMonitor = [NSEvent addLocalMonitorForEventsMatchingMask:mask
+            handler:^NSEvent *(NSEvent *event) {
+                if (wailsMouseGrabWindow == nil || event.window == wailsMouseGrabWindow) {
+                    wailsMouseGrabDelta(event.deltaX, event.deltaY);
+                }
+                return event;
+            }];
+    });
+}
+
+void wails_mouse_grab_stop(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (!wailsMouseGrabbed) return;
+        if (wailsMouseGrabMonitor != nil) {
+            [NSEvent removeMonitor:wailsMouseGrabMonitor];
+            wailsMouseGrabMonitor = nil;
+        }
+        wailsMouseGrabWindow = nil;
+        CGAssociateMouseAndMouseCursorPosition(true);
+        CGDisplayShowCursor(kCGDirectMainDisplay);
+        wailsMouseGrabbed = NO;
+    });
+}
